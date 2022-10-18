84fda5fe5dab4f775b4111ed9bf77cb07b8df4a5
samakshmittal <samakshmittal826@gmail.com>
2022-10-18 10:53:19 +0530

    Create patternsign2.c

---
+++ patternsign2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    for(int i=0; i<=3; i++)
+    {
+        for(int j=0; j<=4; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
