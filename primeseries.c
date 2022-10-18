1b04a7105842f2cb4540e29c8574048d87167371
samakshmittal <samakshmittal826@gmail.com>
2022-10-18 10:53:28 +0530

    Create primeseries.c

---
+++ primeseries.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int n, z=0;
+    printf("Enter number");
+    scanf("%d", &n);
+    for(int j=2; j<=n; j++){
+        int s=(j/2)+1;
+        for(int i=2; i<=s; i++){
+            if(j%i==0 && j!=2){
+                z=1;
+                break;
+            }
+            else
+            z=0;
+    }
+    if(z==0)
+        printf("%d ", j);
+    }
+    
+    return 0;
+}
