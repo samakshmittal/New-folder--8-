2967fa389752b14879d0d37b83fa26af815bdced
samakshmittal <samakshmittal826@gmail.com>
2022-10-21 22:32:26 +0530

    Update patternsign2.c

--- patternsign2.c
+++ patternsign2.c
@@ -2,9 +2,9 @@
 int main()
 {
     int n;
-    for(int i=0; i<=3; i++)
+    for(int i=1; i<=4; i++)
     {
-        for(int j=0; j<=4; j++){
+        for(int j=1; j<=5; j++){
             printf("*");
         }
         printf("\n");
