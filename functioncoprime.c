01753aca94287ad417e6f67938e9ecbbe6383c1d
samakshmittal <samakshmittal826@gmail.com>
2022-10-29 18:58:44 +0530

    Create functioncoprime.c

---
+++ functioncoprime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void coprime(int n){
+    int r, x=0, z=0;
+    int og=n;
+    while(og!=0){
+        r=og%10;
+        og/=10;
+        x=x*10+r;
+    }
+    for(int i=2; i<=n/2; i++){
+        if(n%i==0){
+            if(x%i==0){
+                z=1;
+            }
+        }
+    }
+    if (z==1){
+        printf("%d and %d are not coprime", n, x);
+    }
+    else if (z==0){
+        printf("%d and %d are coprime", n, x);
+    }
+}
+int main(){
+    int n;
+    printf("Enter number");
+    scanf("%d", &n);
+    coprime(n);
+    return 0;
+}
