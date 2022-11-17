ffef02b934caefe202477ca4e636132aa62d6a83
samakshmittal <samakshmittal826@gmail.com>
2022-11-17 15:20:14 +0530

    Update primeseries.c

--- primeseries.c
+++ primeseries.c
@@ -4,7 +4,7 @@ int main(){
     printf("Enter number");
     scanf("%d", &n);
     for(int j=2; j<=n; j++){
-        int s=(j/2)+1;
+        int s=(j/2);
         for(int i=2; i<=s; i++){
             if(j%i==0 && j!=2){
                 z=1;
@@ -12,7 +12,7 @@ int main(){
             }
             else
             z=0;
-    }
+        }
     if(z==0)
         printf("%d ", j);
     }
