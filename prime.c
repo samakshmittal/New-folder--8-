76371f0cab66ad2ac06d132a96d23678b5b8880a
samakshmittal <samakshmittal826@gmail.com>
2022-11-17 15:20:21 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -3,18 +3,24 @@ int main(){
     int n, z=0;
     printf("Enter number");
     scanf("%d", &n);
-    int s=n/2+1;
+    int s=n/2;
     for(int i=2; i<=s; i++){
         if(n%i==0){
             z=1;
             break;
         }
+        else{
+            z=2;
+        }
     }
     if(z==1){
         printf("not prime");
     }
-    else{
+    else if(z==2){
         printf("prime");
+    }
+    else{
+        printf("Invlaid");
     }
     return 0;
 }
