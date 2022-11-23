9a8e3259df5f3d77f13a85555c3fdb63a8d34926
samakshmittal <samakshmittal826@gmail.com>
2022-11-24 03:19:13 +0530

    Update sumarrayrecursion.c

--- sumarrayrecursion.c
+++ sumarrayrecursion.c
@@ -1,18 +1,20 @@
-#include <Stdio.h>
-int add(int n, int *array[n]){
-    if (n==0){
-        return *array[0];
-    }
-    int s=add(n, array[n])+array[n];
-    return s;
-}
+#include <stdio.h>
+int sum(int n, int *array[]);
 int main(){
-    int n, *array[n];
+    int n;
+    int array[1000];
     printf("Enter number of integers in array");
     scanf("%d", &n);
     for (int i=0; i<n; i++){
         scanf("%d", &array[i]);
     }
-    printf("%d", add(n, &array[n]));
+    printf("%d", sum(n, &array[n]));
     return 0;
+}
+int sum(int n, int *array[]){
+    if (n==0){
+        return array[0];
+    }
+    int s=sum(n-1, &array[n-1])+array[n];
+    return s;
 }
