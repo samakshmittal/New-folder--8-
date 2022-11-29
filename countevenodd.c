cd1b99b83490009c3bb490a9a4637f28129b171e
samakshmittal <samakshmittal826@gmail.com>
2022-11-30 01:52:14 +0530

    Create countevenodd.c

---
+++ countevenodd.c
@@ -0,0 +1,43 @@
+#include<stdio.h> 
+int main(){
+int a,b; 
+int i,j;
+int even=0,odd=0;
+printf("Enter hte rows and columns of the matrix \n");
+scanf("%d %d",&a,&b);
+int arr[a][b];
+for(i=1;i<=a;i++){
+    for(j=1;j<=b;j++){ 
+        scanf("%d\n",&arr[i][j]);
+ 
+}
+}
+
+printf("The matrix is \n"); 
+for(i=1;i<=a;i++){ 
+    for(j=1;j<=b;j++){
+printf("%d\t",arr[i][j]);
+
+}
+printf("\n");
+}
+
+for (int i = 1; i <= a; i++)
+{
+
+
+for (int j = 1; j <= b; j++)
+{   
+    if (arr[i][j]%2==0) even++;
+    else odd++;
+    }
+
+
+}
+printf("Even numbers in the array are :%d", even); 
+printf("\n Odd numbers in the array are :%d",odd);
+
+
+
+return 0;
+}
