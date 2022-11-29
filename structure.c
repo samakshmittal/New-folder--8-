672933afa1d5f77684442ed3192502aefae7ad94
samakshmittal <samakshmittal826@gmail.com>
2022-11-29 13:58:05 +0530

    Create structure.c

---
+++ structure.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+struct Product
+ {
+    char pname [100];
+    float per_unit;
+    int quant;
+    float amount;
+ };
+ struct Product obj ;
+ struct Product *ptr=&obj;
+ int main()
+ {
+    int a,b;
+    printf("Enter the product name\n");
+    scanf("%s",ptr->pname );
+    printf("Product name is = %s\n",ptr->pname);
+    printf("Enter the price per unit\n");
+    scanf("%f",&ptr->per_unit);
+    a=ptr->per_unit;
+    printf("Price per unit = %.3f\n",ptr->per_unit);
+    printf("Enter the number of quantities\n");
+    scanf("%d",&ptr->quant);
+    b=ptr->quant;
+    printf("Number of Quantities= %d\n",ptr->quant );
+    ptr->amount=a*b;
+    printf("Amount spend = %.3f\n",ptr->amount);
+    return 0;
+ }
