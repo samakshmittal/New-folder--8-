0cab1df2402851747cd5c77e3ea170f70ab1b4f3
samakshmittal <samakshmittal826@gmail.com>
2022-12-18 22:39:12 +0530

    Create file3.c

---
+++ file3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a=1, b=2, c=3, d;
+    d=a>b<c;
+    printf("%d",d);
+    return 0;
+}#include <stdio.h>
+int main(){
+    float price[3];
+    printf("Enter 3 prices");
+    scanf("%f", &price[0]);
+    scanf("%f", &price[1]);
+    scanf("%f", &price[2]);
+    printf("Cost1 = %f, Cost2 = %f, Cost3 = %f", price[0]+price[0]*(0.18), price[1]+price[1]*(0.18), price[2]+price[2]*(0.18));
+    return 0;
+}
